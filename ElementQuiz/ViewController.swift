3f59878addc65b60f4c861a25469343decbf1948
Salvador <Sagarcia2713@gmail.com>
2022-01-18 15:01:12 -0600

    Add four element images to the Xcassets folder. Create IBOutlets to the image view and answer label. Add an array that contains the names of the four elements. Also add a variable called currentElementIndex to keep track of what element to access from the array.

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let elementList = ["Carbon","Gold","Chlorine","Sodium"]
+    var currentElementIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
